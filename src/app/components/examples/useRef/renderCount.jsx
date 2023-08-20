7648c9bb2daacd8605514fdc49f93afdf18467fe
OlegOTB <olegotb@mail.ru>
2023-08-20 21:55:38 +0300

    test 1

--- src/app/components/examples/useRef/renderCount.jsx
+++ src/app/components/examples/useRef/renderCount.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typografy/smallTitle";
 const RenderCountExample = () => {
+    const [renderCount, setRenderCount] = useState(0);
+    const someRef = useRef("someRef");
+    useEffect(() => {
+        setRenderCount((prevState) => prevState + 1);
+    });
     return (
         <CardWrapper>
             <SmallTitle>Подсчет количества рендеров</SmallTitle>
+            {someRef.current}
+            <p>render count: {renderCount}</p>
         </CardWrapper>
     );
 };
