01813feaea8c22ad6e37760e61561797f5660462
OlegOTB <olegotb@mail.ru>
2023-08-21 13:14:15 +0300

    test 2

--- src/app/components/examples/useRef/renderCount.jsx
+++ src/app/components/examples/useRef/renderCount.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState, useEffect } from "react";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typografy/smallTitle";
+import Divider from "../../common/divider";
 const RenderCountExample = () => {
     const renderCount = useRef(0);
     const [otherState, setOtherState] = useState(false);
@@ -13,6 +14,7 @@ const RenderCountExample = () => {
     return (
         <CardWrapper>
             <SmallTitle>Подсчет количества рендеров</SmallTitle>
+            <Divider />
             <p>render count: {renderCount.current}</p>
             <button className="btn btn-primary" onClick={toggleOtherState}>
                 Toggle other state
--- src/app/components/exercises/useRefExercise.jsx
+++ src/app/components/exercises/useRefExercise.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useRef } from "react";
 import CollapseWrapper from "../common/collapse";
 const UseRefExercise = () => {
+    const divRef = useRef();
+    const handleChange = () => {
+        divRef.current.style.height = "150px";
+        divRef.current.style.width = "80px";
+        divRef.current.children[0].textContent = "text";
+    };
+
     return (
         <CollapseWrapper title="Упражнение">
             <p className="mt-3">
@@ -12,6 +19,7 @@ const UseRefExercise = () => {
                 <li>высота и ширина станут равны 150 и 80 соответственно</li>
             </ul>
             <div
+                ref={divRef}
                 className="bg-primary d-flex flex-row justify-content-center align-items-center rounded"
                 style={{
                     height: 40,
@@ -21,6 +29,12 @@ const UseRefExercise = () => {
             >
                 <small>Блок</small>
             </div>
+            <button
+                className="btn btn-primary btn-sm m-2"
+                onClick={handleChange}
+            >
+                Изменить блок
+            </button>
         </CollapseWrapper>
     );
 };
